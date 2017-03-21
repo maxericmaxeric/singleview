96036be4365abdb25cf32a4c5e185da035770866
JohnHe2015 <xiaozhemu@163.com>
2017-03-21 17:55:55 +0800

    增加客户模糊查询

--- src/main/java/com/ecsolutions/controller/DEPOSIT_ACCOUNT_TYPE_BALANCE_LCY_SUM_Controller.java
+++ src/main/java/com/ecsolutions/controller/DEPOSIT_ACCOUNT_TYPE_BALANCE_LCY_SUM_Controller.java
@@ -16,7 +16,7 @@ public class DEPOSIT_ACCOUNT_TYPE_BALANCE_LCY_SUM_Controller {
     @Autowired
     private DEPOSIT_ACCOUNT_TYPE_BALANCE_LCY_SUM_Service dEPOSIT_ACCOUNT_TYPE_BALANCE_LCY_SUM_Service;
 
-    @CrossOrigin(origins = "*", maxAge = 3600)
+        @CrossOrigin(origins = "*", maxAge = 3600)
     @RequestMapping(method = RequestMethod.GET, value = "/{customer_code}")
     public List<DEPOSIT_ACCOUNT_TYPE_BALANCE_LCY_SUM_Entity> getDEPOSIT_ACCOUNT_TYPE_BALANCE_LCY_SUMByCustomer_code(@PathVariable("customer_code") String customer_code) {
         List<DEPOSIT_ACCOUNT_TYPE_BALANCE_LCY_SUM_Entity> dEPOSIT_ACCOUNT_TYPE_BALANCE_LCY_SUM_Entitys = dEPOSIT_ACCOUNT_TYPE_BALANCE_LCY_SUM_Service.findDEPOSIT_ACCOUNT_TYPE_BALANCE_LCY_SUMByCustomer_code(customer_code);
